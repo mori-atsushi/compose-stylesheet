69febfdc67d2a54737421e22f6790db810d66a10
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-08 21:04:40 +0900

    Mark the models as immutable

--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/StyleSheet.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/StyleSheet.kt
@@ -1,12 +1,14 @@
 package com.moriatsushi.compose.stylesheet
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 import com.moriatsushi.compose.stylesheet.color.ColorToken
 
 /**
  * A style sheet that contains style definitions.
  */
+@Immutable
 class StyleSheet internal constructor(
     private val colors: Map<ColorToken, ColorToken> = emptyMap(),
 ) {
@@ -14,6 +16,17 @@ class StyleSheet internal constructor(
         val nextToken = colors[token] ?: return token.default
         return getColor(nextToken)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is StyleSheet) return false
+
+        return colors == other.colors
+    }
+
+    override fun hashCode(): Int = colors.hashCode()
+
+    override fun toString(): String = "StyleSheet(colors=$colors)"
 
     companion object {
         /**
--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/color/ColorToken.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/color/ColorToken.kt
@@ -1,10 +1,12 @@
 package com.moriatsushi.compose.stylesheet.color
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 
 /**
  * A token that represents a color.
  */
+@Immutable
 class ColorToken(
     private val name: String,
     internal val default: Color,
