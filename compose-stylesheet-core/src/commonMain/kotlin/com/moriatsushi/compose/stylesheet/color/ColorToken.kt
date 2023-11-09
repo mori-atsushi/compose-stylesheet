028229e589c4f99b9eae79e9f037a5152c7efa65
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-09 21:50:56 +0900

    Add Unspecified for ColorToken

--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/color/ColorToken.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/color/ColorToken.kt
@@ -2,7 +2,9 @@ package com.moriatsushi.compose.stylesheet.color
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.isSpecified
 import com.moriatsushi.compose.stylesheet.StyleSheet
 
 /**
@@ -14,9 +16,29 @@ class ColorToken(
     internal val default: Color,
 ) {
     override fun toString(): String = "ColorToken($name, default=$default)"
+
+    companion object {
+        /**
+         * Constant for an unspecified [ColorToken].
+         */
+        val Unspecified = ColorToken("<unspecified>", Color.Unspecified)
+    }
 }
 
-internal fun Color.toToken(): ColorToken = ColorToken(toString(), this)
+internal fun Color.toToken(): ColorToken =
+    if (isSpecified) ColorToken("<source>", this) else ColorToken.Unspecified
+
+/**
+ * `false` when this is [Color.Unspecified].
+ */
+@Stable
+val ColorToken.isSpecified: Boolean get() = default != Color.Unspecified
+
+/**
+ * `true` when this is [Color.Unspecified].
+ */
+@Stable
+val ColorToken.isUnspecified: Boolean get() = default == Color.Unspecified
 
 /**
  * Returns the color corresponding to the given [ColorToken].
