5e869938c194cc505881056b4fe4fc9858e7f5ca
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-09 23:01:54 +0900

    Change component symbols

--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/Component.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/Component.kt
@@ -1,9 +1,28 @@
 package com.moriatsushi.compose.stylesheet
 
 /**
- * An interface for marking a class as a component, which has a style and a style builder.
+ * A symbol of a component, which has a style and a style builder.
  */
 interface Component<S : ComponentStyle, SB : ComponentStyleBuilder<S>> {
     val defaultStyle: S
     fun createBuilder(): SB
+}
+
+/**
+ * Creates a symbol of a component.
+ */
+fun <S : ComponentStyle, SB : ComponentStyleBuilder<S>> Component(
+    name: String,
+    defaultStyle: S,
+    createBuilder: () -> SB,
+): Component<S, SB> = ComponentImpl(name, defaultStyle, createBuilder)
+
+private class ComponentImpl<S : ComponentStyle, SB : ComponentStyleBuilder<S>>(
+    private val name: String,
+    override val defaultStyle: S,
+    private val builder: () -> SB,
+) : Component<S, SB> {
+    override fun createBuilder(): SB = builder()
+
+    override fun toString(): String = "Component($name)"
 }
