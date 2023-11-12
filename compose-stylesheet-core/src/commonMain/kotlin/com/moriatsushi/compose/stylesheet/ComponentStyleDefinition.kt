6fc1531abd1e108acb6697bcada398be2fa80d5e
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-12 17:21:29 +0900

    Use a data class for ComponentStyleDefinition

--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/ComponentStyleDefinition.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/ComponentStyleDefinition.kt
@@ -2,7 +2,7 @@ package com.moriatsushi.compose.stylesheet
 
 import com.moriatsushi.compose.stylesheet.tag.Tag
 
-internal class ComponentStyleDefinition<T : ComponentStyle>(
+internal data class ComponentStyleDefinition<T : ComponentStyle>(
     val commonStyle: T,
     val tagStyles: Map<Tag<T>, T> = emptyMap(),
 ) {
@@ -17,25 +17,6 @@ internal class ComponentStyleDefinition<T : ComponentStyle>(
             commonStyle = commonStyle,
             tagStyles = tagStyles + (tag to style),
         )
-
-    override fun toString(): String = "ComponentStyleDefinition(" +
-        "commonStyle=$commonStyle," +
-        "tagStyles=$tagStyles)"
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is ComponentStyleDefinition<*>) return false
-
-        if (commonStyle != other.commonStyle) return false
-        if (tagStyles != other.tagStyles) return false
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = commonStyle.hashCode()
-        result = 31 * result + tagStyles.hashCode()
-        return result
-    }
 
     companion object {
         fun <T : ComponentStyle> fromTag(
