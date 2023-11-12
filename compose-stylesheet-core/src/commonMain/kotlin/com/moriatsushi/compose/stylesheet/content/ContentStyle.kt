55c03a98ccb80d77ced2cf048bbe051b09926f0a
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-12 16:20:10 +0900

    Add parameters to ContentStyle

--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/content/ContentStyle.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/content/ContentStyle.kt
@@ -3,6 +3,25 @@ package com.moriatsushi.compose.stylesheet.content
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontSynthesis
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.LocaleList
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.style.Hyphens
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.text.style.TextGeometricTransform
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.style.TextMotion
+import androidx.compose.ui.unit.TextUnit
 import com.moriatsushi.compose.stylesheet.token.Token
 
 /**
@@ -12,6 +31,29 @@ import com.moriatsushi.compose.stylesheet.token.Token
 @Stable
 sealed interface ContentStyle {
     val color: Token<Color>?
+    val fontSize: Token<TextUnit>?
+    val fontWeight: Token<FontWeight?>?
+    val fontStyle: Token<FontStyle?>?
+    val fontSynthesis: Token<FontSynthesis?>?
+    val fontFamily: Token<FontFamily?>?
+    val fontFeatureSettings: Token<String?>?
+    val letterSpacing: Token<TextUnit>?
+    val baselineShift: Token<BaselineShift?>?
+    val textGeometricTransform: Token<TextGeometricTransform?>?
+    val localeList: Token<LocaleList?>?
+    val textBackground: Token<Color?>?
+    val textDecoration: Token<TextDecoration?>?
+    val textShadow: Token<Shadow?>?
+    val textDrawStyle: Token<DrawStyle?>?
+    val textAlign: Token<TextAlign?>?
+    val textDirection: Token<TextDirection?>?
+    val lineHeight: Token<TextUnit>?
+    val textIndent: Token<TextIndent?>?
+    val platformTextStyle: Token<PlatformTextStyle?>?
+    val lineHeightStyle: Token<LineHeightStyle?>?
+    val lineBreak: Token<LineBreak?>?
+    val hyphens: Token<Hyphens?>?
+    val textMotion: Token<TextMotion?>?
 
     companion object {
         /**
@@ -29,11 +71,80 @@ fun ContentStyle(builder: ContentStyleBuilder.() -> Unit): ContentStyle =
 
 internal fun ContentStyle(
     color: Token<Color>? = null,
+    fontSize: Token<TextUnit>? = null,
+    fontWeight: Token<FontWeight?>? = null,
+    fontStyle: Token<FontStyle?>? = null,
+    fontSynthesis: Token<FontSynthesis?>? = null,
+    fontFamily: Token<FontFamily?>? = null,
+    fontFeatureSettings: Token<String?>? = null,
+    letterSpacing: Token<TextUnit>? = null,
+    baselineShift: Token<BaselineShift?>? = null,
+    textGeometricTransform: Token<TextGeometricTransform?>? = null,
+    localeList: Token<LocaleList?>? = null,
+    textBackground: Token<Color?>? = null,
+    textDecoration: Token<TextDecoration?>? = null,
+    textShadow: Token<Shadow?>? = null,
+    textDrawStyle: Token<DrawStyle?>? = null,
+    textAlign: Token<TextAlign?>? = null,
+    textDirection: Token<TextDirection?>? = null,
+    lineHeight: Token<TextUnit>? = null,
+    textIndent: Token<TextIndent?>? = null,
+    platformTextStyle: Token<PlatformTextStyle?>? = null,
+    lineHeightStyle: Token<LineHeightStyle?>? = null,
+    lineBreak: Token<LineBreak?>? = null,
+    hyphens: Token<Hyphens?>? = null,
+    textMotion: Token<TextMotion?>? = null,
 ): ContentStyle = ContentStyleImpl(
     color = color,
+    fontSize = fontSize,
+    fontWeight = fontWeight,
+    fontStyle = fontStyle,
+    fontSynthesis = fontSynthesis,
+    fontFamily = fontFamily,
+    fontFeatureSettings = fontFeatureSettings,
+    letterSpacing = letterSpacing,
+    baselineShift = baselineShift,
+    textGeometricTransform = textGeometricTransform,
+    localeList = localeList,
+    textBackground = textBackground,
+    textDecoration = textDecoration,
+    textShadow = textShadow,
+    textDrawStyle = textDrawStyle,
+    textAlign = textAlign,
+    textDirection = textDirection,
+    lineHeight = lineHeight,
+    textIndent = textIndent,
+    platformTextStyle = platformTextStyle,
+    lineHeightStyle = lineHeightStyle,
+    lineBreak = lineBreak,
+    hyphens = hyphens,
+    textMotion = textMotion,
 )
 
 @Immutable
 private data class ContentStyleImpl(
     override val color: Token<Color>?,
+    override val fontSize: Token<TextUnit>?,
+    override val fontWeight: Token<FontWeight?>?,
+    override val fontStyle: Token<FontStyle?>?,
+    override val fontSynthesis: Token<FontSynthesis?>?,
+    override val fontFamily: Token<FontFamily?>?,
+    override val fontFeatureSettings: Token<String?>?,
+    override val letterSpacing: Token<TextUnit>?,
+    override val baselineShift: Token<BaselineShift?>?,
+    override val textGeometricTransform: Token<TextGeometricTransform?>?,
+    override val localeList: Token<LocaleList?>?,
+    override val textBackground: Token<Color?>?,
+    override val textDecoration: Token<TextDecoration?>?,
+    override val textShadow: Token<Shadow?>?,
+    override val textDrawStyle: Token<DrawStyle?>?,
+    override val textAlign: Token<TextAlign?>?,
+    override val textDirection: Token<TextDirection?>?,
+    override val lineHeight: Token<TextUnit>?,
+    override val textIndent: Token<TextIndent?>?,
+    override val platformTextStyle: Token<PlatformTextStyle?>?,
+    override val lineHeightStyle: Token<LineHeightStyle?>?,
+    override val lineBreak: Token<LineBreak?>?,
+    override val hyphens: Token<Hyphens?>?,
+    override val textMotion: Token<TextMotion?>?,
 ) : ContentStyle
--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/content/ContentStyleBuilder.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/content/ContentStyleBuilder.kt
@@ -1,6 +1,25 @@
 package com.moriatsushi.compose.stylesheet.content
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontSynthesis
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.LocaleList
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.style.Hyphens
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.text.style.TextGeometricTransform
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.style.TextMotion
+import androidx.compose.ui.unit.TextUnit
 import com.moriatsushi.compose.stylesheet.StyleBuilder
 import com.moriatsushi.compose.stylesheet.StyleSheetBuilderMarker
 import com.moriatsushi.compose.stylesheet.token.TokenSetter
@@ -10,16 +29,82 @@ import com.moriatsushi.compose.stylesheet.token.TokenSetter
  */
 @StyleSheetBuilderMarker
 class ContentStyleBuilder internal constructor() : StyleBuilder<ContentStyle> {
-    /**
-     * A content color, which is used for text and icons.
-     */
     val color: TokenSetter<Color> = TokenSetter()
+    val fontSize: TokenSetter<TextUnit> = TokenSetter()
+    val fontWeight: TokenSetter<FontWeight?> = TokenSetter()
+    val fontStyle: TokenSetter<FontStyle?> = TokenSetter()
+    val fontSynthesis: TokenSetter<FontSynthesis?> = TokenSetter()
+    val fontFamily: TokenSetter<FontFamily?> = TokenSetter()
+    val fontFeatureSettings: TokenSetter<String?> = TokenSetter()
+    val letterSpacing: TokenSetter<TextUnit> = TokenSetter()
+    val baselineShift: TokenSetter<BaselineShift?> = TokenSetter()
+    val textGeometricTransform: TokenSetter<TextGeometricTransform?> = TokenSetter()
+    val localeList: TokenSetter<LocaleList?> = TokenSetter()
+    val textBackground: TokenSetter<Color?> = TokenSetter()
+    val textDecoration: TokenSetter<TextDecoration?> = TokenSetter()
+    val textShadow: TokenSetter<Shadow?> = TokenSetter()
+    val textDrawStyle: TokenSetter<DrawStyle?> = TokenSetter()
+    val textAlign: TokenSetter<TextAlign?> = TokenSetter()
+    val textDirection: TokenSetter<TextDirection?> = TokenSetter()
+    val lineHeight: TokenSetter<TextUnit> = TokenSetter()
+    val textIndent: TokenSetter<TextIndent?> = TokenSetter()
+    val platformTextStyle: TokenSetter<PlatformTextStyle?> = TokenSetter()
+    val lineHeightStyle: TokenSetter<LineHeightStyle?> = TokenSetter()
+    val lineBreak: TokenSetter<LineBreak?> = TokenSetter()
+    val hyphens: TokenSetter<Hyphens?> = TokenSetter()
+    val textMotion: TokenSetter<TextMotion?> = TokenSetter()
 
     override fun plusAssign(other: ContentStyle) {
         color += other.color
+        fontSize += other.fontSize
+        fontWeight += other.fontWeight
+        fontStyle += other.fontStyle
+        fontSynthesis += other.fontSynthesis
+        fontFamily += other.fontFamily
+        fontFeatureSettings += other.fontFeatureSettings
+        letterSpacing += other.letterSpacing
+        baselineShift += other.baselineShift
+        textGeometricTransform += other.textGeometricTransform
+        localeList += other.localeList
+        textBackground += other.textBackground
+        textDecoration += other.textDecoration
+        textShadow += other.textShadow
+        textDrawStyle += other.textDrawStyle
+        textAlign += other.textAlign
+        textDirection += other.textDirection
+        lineHeight += other.lineHeight
+        textIndent += other.textIndent
+        platformTextStyle += other.platformTextStyle
+        lineHeightStyle += other.lineHeightStyle
+        lineBreak += other.lineBreak
+        hyphens += other.hyphens
+        textMotion += other.textMotion
     }
 
     override fun build(): ContentStyle = ContentStyle(
         color = color.token,
+        fontSize = fontSize.token,
+        fontWeight = fontWeight.token,
+        fontStyle = fontStyle.token,
+        fontSynthesis = fontSynthesis.token,
+        fontFamily = fontFamily.token,
+        fontFeatureSettings = fontFeatureSettings.token,
+        letterSpacing = letterSpacing.token,
+        baselineShift = baselineShift.token,
+        textGeometricTransform = textGeometricTransform.token,
+        localeList = localeList.token,
+        textBackground = textBackground.token,
+        textDecoration = textDecoration.token,
+        textShadow = textShadow.token,
+        textDrawStyle = textDrawStyle.token,
+        textAlign = textAlign.token,
+        textDirection = textDirection.token,
+        lineHeight = lineHeight.token,
+        textIndent = textIndent.token,
+        platformTextStyle = platformTextStyle.token,
+        lineHeightStyle = lineHeightStyle.token,
+        lineBreak = lineBreak.token,
+        hyphens = hyphens.token,
+        textMotion = textMotion.token,
     )
 }
