ab1543f13068cc50b02ce92981caf302845ac8a1
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-12 16:00:55 +0900

    Use a data class for ContentStyle

--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/ContentStyle.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/ContentStyle.kt
@@ -1,25 +1,12 @@
 package com.moriatsushi.compose.stylesheet
 
-import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 import com.moriatsushi.compose.stylesheet.token.Token
 
-@Immutable
-class ContentStyle(
-    val color: Token<Color>? = null,
-) {
-    override fun hashCode(): Int = color.hashCode()
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is ContentStyle) return false
-
-        if (color != other.color) return false
-        return true
-    }
-
-    override fun toString(): String = "ContentStyle(" +
-        "color=$color)"
+@Stable
+sealed interface ContentStyle {
+    val color: Token<Color>?
 
     companion object {
         /**
@@ -34,3 +21,13 @@ class ContentStyle(
  */
 fun ContentStyle(builder: ContentStyleBuilder.() -> Unit): ContentStyle =
     ContentStyleBuilder().apply(builder).build()
+
+internal fun ContentStyle(
+    color: Token<Color>? = null,
+): ContentStyle = ContentStyleImpl(
+    color = color,
+)
+
+private data class ContentStyleImpl(
+    override val color: Token<Color>?,
+) : ContentStyle
