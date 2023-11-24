3f3d43fc576512ffb0f1ba409dd0431f4238fb86
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-11-24 11:28:17 +0900

    Refactor TokenSetter

--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/StyleBuilder.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/StyleBuilder.kt
@@ -1,28 +1,9 @@
 package com.moriatsushi.compose.stylesheet
 
-import com.moriatsushi.compose.stylesheet.token.Token
-import com.moriatsushi.compose.stylesheet.token.TokenSetter
-
 /**
  * An interface for building a [style][T].
  */
 interface StyleBuilder<T> {
-    /**
-     * Sets the given [token] to [this].
-     */
-    operator fun <T> TokenSetter<T>.plusAssign(token: Token<T>?) {
-        if (token != null) {
-            this.token = token
-        }
-    }
-
-    /**
-     * Sets the given [value] to [this].
-     */
-    operator fun <T> TokenSetter<T>.plusAssign(value: T) {
-        this.token = Token(value)
-    }
-
     /**
      * Merges this style with the given [other] style.
      */
--- compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/token/TokenSetter.kt
+++ compose-stylesheet-core/src/commonMain/kotlin/com/moriatsushi/compose/stylesheet/token/TokenSetter.kt
@@ -3,6 +3,23 @@ package com.moriatsushi.compose.stylesheet.token
 import com.moriatsushi.compose.stylesheet.component.StyleSheetComponentApi
 
 class TokenSetter<T> @StyleSheetComponentApi constructor() {
+    @StyleSheetComponentApi
     var token: Token<T>? = null
-        internal set
+        private set
+
+    /**
+     * Sets the given [token].
+     */
+    operator fun plusAssign(token: Token<T>?) {
+        if (token != null) {
+            this.token = token
+        }
+    }
+
+    /**
+     * Sets the given [value].
+     */
+    operator fun plusAssign(value: T) {
+        this.token = Token(value)
+    }
 }
